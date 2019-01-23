087c38b67bec2ffbc513239fb710d3868e5f4d47
Mauro Bernal <maurobernal@192-168-1-13.tpgi.com.au>
2019-01-23 20:35:05 +1100

    bootstrap files and helpers

---
+++ bootstrap.exclude.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+PATH_TO_PROJECTS=~/Projects
+PATH_TO_PLAYGROUND=~/Playground
+
+# Initialize a few things
+init () {
+	echo "Making a Projects folder in $PATH_TO_PROJECTS if it doesn't already exist"
+	mkdir -p "$PATH_TO_PROJECTS"
+	echo "Making a Playground folder in $PATH_TO_PLAYGROUND if it doesn't already exist"
+	mkdir -p "$PATH_TO_PLAYGROUND"
+}
+
+# TODO : Delete symlinks to deleted files
+# Is this where rsync shines?
+# TODO - add support for -f and --force
+link () {
+	echo "This utility will symlink the files in this repo to the home directory"
+	echo "Proceed? (y/n)"
+	read resp
+	# TODO - regex here?
+	if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
+		for file in $( ls -A | grep -vE '\.exclude*|\.git$|\.gitignore|.*.md' ) ; do
+			ln -sv "$PWD/$file" "$HOME"
+		done
+		# TODO: source files here?
+		echo "Symlinking complete"
+	else
+		echo "Symlinking cancelled by user"
+		return 1
+	fi
+}
+
+install_tools () {
+	if [ $( echo "$OSTYPE" | grep 'darwin' ) ] ; then
+		echo "This utility will install useful utilities using Homebrew"
+		echo "Proceed? (y/n)"
+		read resp
+		# TODO - regex here?
+		if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
+            xcode-select --install
+
+			echo "Installing useful stuff using brew. This may take a while..."
+			sh brew.exclude.sh
+		else
+			echo "Brew installation cancelled by user"
+		fi
+	else
+		echo "Skipping installations using Homebrew because MacOS was not detected..."
+	fi
+}
+
+init
+link
+install_tools
